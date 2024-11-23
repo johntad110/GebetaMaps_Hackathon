ebbf148b3b0e37c8bcdec0fa488c2b0f82ee52a0
John Tad <johntad110@gmail.com>
2024-11-23 22:41:23 +0300

    add palce holder services page

---
+++ landscoper/src/app/[lang]/services/page.tsx
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+        <div>
+            <h1 className="min-h-screen flex justify-center items-center font-bold text-5xl font-[family-name:var(--font-funnel-display)]">
+                This page was built in the Future.
+            </h1>
+        </div>
+    )
+}
