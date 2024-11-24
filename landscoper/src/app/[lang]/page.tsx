07ef558fb3cf02fd4b2d2f2f3066e9fb9d5a7d36
John Tad <johntad110@gmail.com>
2024-11-24 08:58:49 +0300

    resolve params promise

--- landscoper/src/app/[lang]/page.tsx
+++ landscoper/src/app/[lang]/page.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import { getDictionary } from "./dictionaries";
 
 interface HomeProp {
-  params: {
+  params: Promise<{
     lang: string
-  }
+  }>
 }
 
-export default async function Home({ params: { lang } }: HomeProp) {
+export default async function Home({ params }: HomeProp) {
+  const { lang } = await params;
   const dict = await getDictionary(lang)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-funnel-display)]">
